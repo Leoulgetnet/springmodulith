83e614a03b1cdd475c9dcfb3ad9f1b72a8118d43
Leoulgetnet <132510436+Leoulgetnet@users.noreply.github.com>
2026-06-24 20:17:47 +0300

    Initial commit

---
+++ src/main/java/com/leoulgetnet/springmodulith/SpringmodulithApplication.java
@@ -0,0 +1,13 @@
+package com.leoulgetnet.springmodulith;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmodulithApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringmodulithApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leoulgetnet/springmodulith/SpringmodulithApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leoulgetnet.springmodulith;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringmodulithApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
